f2619327aa3c7e82afc684c7a27d3f2aadf741dd
Eli <ego.8152001@gmail.com>
2024-10-17 11:47:16 +0800

    feat: added and removed some files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client' 
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App/>
+    </ChakraProvider>
   </StrictMode>,
 )
